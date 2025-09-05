b68bc03774283c0fef94438004dfb3e33cb36b54
joshwaller-oe <josh.waller@orbiseducation.com>
2025-09-05 16:08:54 -0400

    Fixes vite config setup and postcss plugin usage

--- vite.config.ts
+++ vite.config.ts
@@ -1,29 +1,32 @@
 import { defineConfig, loadEnv } from "vite";
+import type { ConfigEnv } from "vite";
 import react from "@vitejs/plugin-react";
 import postcssImport from "postcss-import";
 import postcssPresetEnv from "postcss-preset-env";
 import postcssNested from "postcss-nested";
 import autoprefixer from "autoprefixer";
 
-export default defineConfig(({ mode }) => {
+export default defineConfig(({ mode }: ConfigEnv) => {
   // * Load env file based on `mode` in the current working directory.
   // * Set the third parameter to '' to load all env regardless of the
   // * `VITE_` prefix.
   const env = loadEnv(mode, process.cwd(), "");
+  const port = Number(env.PORT);
+
   return {
     base: "./",
     plugins: [
       react()
     ],
     server: {
-      port: env.PORT
+      port
     },
     css: {
       postcss: {
         plugins: [
-          postcssImport,
-          postcssNested,
-          autoprefixer,
+          postcssImport(),
+          postcssNested(),
+          autoprefixer(),
           postcssPresetEnv({ stage: 1 })
         ]
       }
