4ac457273a8d0e4d1177f2b5d4b01897b8380d36
mrfisher-oe <michael.fisher@orbiseducation.com>
2025-09-05 15:32:58 -0400

    code clean up

--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,5 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
+import { StrictMode } from "react"
+import { createRoot } from "react-dom/client"
 import { PublicClientApplication, EventType, EventMessage, AuthenticationResult } from "@azure/msal-browser";
 import { MsalProvider } from "@azure/msal-react";
 import { msalConfig } from "./utilities/authenticationConfig";
@@ -9,12 +9,6 @@ import "./css/index.css";
 // * https://stackoverflow.com/questions/66384368/how-is-it-possible-to-access-homepage-from-package-json-in-a-react-app -- 12/17/2021 MF
 // * Using Vite requires a different syntax. -- 09/22/2023 MF
 import { version, copyrightYear } from "../package.json";
-const applicationVersion = version;
-
-// const componentName = "index";
-
-// console.log(componentName, "applicationVersion", applicationVersion);
-// console.log(componentName, "copyrightYear", copyrightYear);
 
 export const msalInstance = new PublicClientApplication(msalConfig);
 
@@ -34,24 +28,27 @@ msalInstance.initialize().then(() => {
   msalInstance.addEventCallback((event: EventMessage) => {
 
     if (event.eventType === EventType.LOGIN_SUCCESS) {
+
       const result = event.payload as AuthenticationResult;
 
       if (result?.account) {
+
         msalInstance.setActiveAccount(result.account);
-      }
+
+      };
 
     };
 
   });
 
-  const root = ReactDOM.createRoot(document.getElementById("root"));
+  const root = createRoot(document.getElementById("root"));
 
   root.render(
-    <React.StrictMode>
+    <StrictMode>
       <MsalProvider instance={msalInstance}>
-        <App applicationVersion={applicationVersion} copyrightYear={copyrightYear} />
+        <App applicationVersion={version} copyrightYear={copyrightYear} />
       </MsalProvider>
-    </React.StrictMode>
+    </StrictMode>
   );
 
 });
