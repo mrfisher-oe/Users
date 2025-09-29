8fd06e4748b9e0fcf024f522cc524b5ec542aaf2
mrfisher-oe <michael.fisher@orbiseducation.com>
2025-09-29 15:40:36 -0400

    user form updates

--- src/components/Users.tsx
+++ src/components/Users.tsx
@@ -1192,20 +1192,17 @@ const Users = ({ processTransactionUserRequest }: UserProps) => {
 
               <button type="button" className="btn btn-dark-gray" onClick={() => { loadRecord(currentUser); dispatch(clearMessages()); setInlineErrors({ txtUsername: "", txtFirstName: "", txtLastName: "", txtEmail: "", ddPartnerSiteID: "" }); }}>Reset</button>
 
+              {currentUser.active ?
+
+                <button type="button" className="btn btn-danger" onClick={deleteRecord}>Delete</button>
+
+                :
+
+                <button type="button" className="btn btn-success" onClick={enableRecord}>Enable</button>
+
+              }
+
               <button type="button" className="btn btn-outline" onClick={() => { setCurrentUser(null); dispatch(setIsFormOpen(false)); dispatch(clearMessages()); setInlineErrors({ txtUsername: "", txtFirstName: "", txtLastName: "", txtEmail: "", ddPartnerSiteID: "" }); }}>Cancel</button>
-
-              {!isEmpty(userID) ?
-
-                <>
-
-                  <button type="button" className="btn btn-danger" onClick={() => { deleteRecord(); }} disabled={currentUser.active !== true}>Delete</button>
-
-                  <button type="button" className="btn btn-success" onClick={() => { enableRecord(); }} disabled={currentUser.active}>Enable</button>
-
-                </>
-
-                : null}
-
 
             </div>
 
